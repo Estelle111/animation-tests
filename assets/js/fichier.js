ed44703347260d53da5a0335b90b355dcef98171
estelle mol <estel@MacBook-Pro-de-estelle.local>
2019-03-06 18:00:08 +0100

    adding few animations

---
+++ assets/js/fichier.js
@@ -0,0 +1,21 @@
+/* Read more */
+
+/* $(document).ready(function(){
+    $(".button").click(function () {
+       var div= $("."+this.name);
+  div.toggle("slow").siblings().hide("slow");
+});
+});  
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+     e.style.display = 'none';
+  else
+     e.style.display = 'block';
+} */
+
+$("#skills").click(function () {
+  if ($(".mytext").is(":visible")) $(".mytext").hide()
+  else $(".mytext").show();
+});
